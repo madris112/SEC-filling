87cb825ea6ac106c74cd122cda1dc4f66e3feaf8
StrixROX <40167899+StrixROX@users.noreply.github.com>
2022-03-19 09:06:25 +0530

    init react app

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Typography from '@mui/material/Typography'
+
+
+const App = () => {
+    return (
+        <div>
+            <Typography variant="h1">Hello, world!</Typography>
+        </div>
+    )
+}
+
+export default App 
