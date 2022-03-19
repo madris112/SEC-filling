69fd9a4becf5b94a658a970f14ea338bbfa2c57c
Aritra Bhaduri <92646038+CodingwithMe123@users.noreply.github.com>
2022-03-19 23:55:48 +0530

    done

--- sec/src/App.js
+++ sec/src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 import DebtChart from './Charts/DebtChart';
 import AssetChart from './Charts/AssetCharts';
 import CashFlowChart from './Charts/CashFlowChart'
+import FutureGrowth from './Charts/FutureGrowth'
 
 function App() {
   return (
     <div className="App">
       {/* <DebtChart company='CIK0000001800' /> */}
-      <AssetChart company='CIK0000001800'/>
+      <AssetChart company='CIK0000001750'/>
       {/* <CashFlowChart company='CIK0000001800'/> */}
+      <FutureGrowth company='CIK0000001800'/>
     </div>
   );
 }
---
+++ sec/src/Charts/FutureGrowth.js
@@ -0,0 +1,128 @@
+import React, { useState, useEffect } from 'react'
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+
+import { Line } from 'react-chartjs-2';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+
+const LineChart = (props) => {
+  const [chart, setChart] = useState({})
+  var data
+  if(props.company === 'CIK0000001750'){ //recent to old
+    data=[1626500000
+        , 1956700000
+        , 1573300000
+    ]
+  }else if(props.company === 'CIK0000001800'){ 
+    data=[86770000000
+        , 81159000000
+        , 73520000000
+    ]
+  }else if(props.company === 'CIK0000001961'){
+    data=[57848925
+        ,
+        54650883
+,
+52441181
+
+    ]
+  }else if(props.company === 'CIK0000002034'){
+    data=[830157000
+        ,
+        900181000
+,
+192042000
+]
+  }
+
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       await fetch(`https://data.sec.gov/api/xbrl/companyconcept/CIK0001708176/us-gaap/LongTermDebt.json`, {
+//         method: 'GET',
+//         headers: {
+//         }
+//       })
+//         .then((response) => {
+//           console.log("api is  accessed")
+//             // response.json().then((json) => {
+//             //   console.log(json);
+//             //   // setChart(json.data)
+//             // });
+//             console.log(response)
+          
+//         }).catch((error) => {
+//           console.log(error);
+//         });
+//     };
+//     fetchData()
+//   }, [])
+
+  console.log("chart", chart);
+
+  var data = {
+    labels: ['2021', '2020', '2019'],
+    datasets: [{
+      label: `Future Growth Potential - Assets`,
+      data: data,
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)'
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)'
+      ],
+      borderWidth: 1
+    }]
+  };
+
+  var options = {
+    maintainAspectRatio: false,
+    scales: {
+    },
+    legend: {
+      labels: {
+        fontSize: 25,
+      },
+    },
+  }
+
+  return (
+    <div>
+      <Line
+        data={data}
+        height={400}
+        options={options}
+
+      />
+    </div>
+  )
+}
+
+export default LineChart
