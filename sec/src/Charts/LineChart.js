8706d9e3d37380b1c091a67b8e0584f54ae5b68d
Aritra Bhaduri <92646038+CodingwithMe123@users.noreply.github.com>
2022-03-19 21:17:39 +0530

    created chart

---
+++ sec/src/Charts/LineChart.js
@@ -0,0 +1,102 @@
+import React, { useState, useEffect } from 'react'
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+
+import { Line } from 'react-chartjs-2';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+
+const LineChart = () => {
+  const [chart, setChart] = useState({})
+
+
+
+  useEffect(() => {
+    const fetchData = async () => {
+      await fetch(`data.sec.gov/api/xbrl/companyconcept/CIK0001708176/us-gaap/LongTermDebt.json`, {
+        method: 'GET',
+        headers: {
+        }
+      })
+        .then((response) => {
+          console.log("api is  accessed")
+            // response.json().then((json) => {
+            //   console.log(json);
+            //   // setChart(json.data)
+            // });
+            console.log(response)
+          
+        }).catch((error) => {
+          console.log(error);
+        });
+    };
+    fetchData()
+  }, [])
+
+  console.log("chart", chart);
+  var data = {
+    labels: chart?.coins?.map(x => x.name),
+    datasets: [{
+      label: `${chart?.coins?.length} Coins Available`,
+      data: chart?.coins?.map(x => x.price),
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)'
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)'
+      ],
+      borderWidth: 1
+    }]
+  };
+
+  var options = {
+    maintainAspectRatio: false,
+    scales: {
+    },
+    legend: {
+      labels: {
+        fontSize: 25,
+      },
+    },
+  }
+
+  return (
+    <div>
+      <Line
+        data={data}
+        height={400}
+        options={options}
+
+      />
+    </div>
+  )
+}
+
+export default LineChart
